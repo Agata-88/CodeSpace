ef25d0e111929d60c66e57de5b33a4e95cc6f595
Agata Sierzega <AgataSierzega@gmail.com>
2023-12-06 22:29:32 +0000

    inheritance example
    strings and arrays

---
+++ challenge2OOP/inheritance.js
@@ -0,0 +1,39 @@
+class User {
+    //Class constructor
+    constructor(){
+        //Initialise the userName
+        this._userName = "";
+    }
+
+//Setters
+set userName(userName) {
+    this._userName = userName;
+    }
+}
+
+//Admin class
+class Admin extends User {
+    //Class constructor
+    constructor(){
+        super();
+    }
+
+    //ExpressYourRole() method
+    explressYourRole() {
+        return "Admin";
+    }
+
+    //sayHello()method
+    sayHello(){
+        return `Hello admin, ${this._userName}`;
+    }
+}
+    //create object () of the class Admin
+    let admin = new Admin();
+
+    //Set user name to "Balthazar"
+    admin.userName = "Balthazar";
+
+    //Show info to the user
+    console.log(admin.sayHello());
+
